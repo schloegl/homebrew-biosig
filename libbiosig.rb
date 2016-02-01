959f46861412833c0802fe556462c4dbe456a32c
Alois Schloegl <alois.schloegl@ist.ac.at>
2016-02-01 21:21:15 +0100

    pkg libbiosig: fix typo

--- libbiosig.rb
+++ libbiosig.rb
@@ -2,7 +2,7 @@ class Libbiosig < Formula
   homepage "http://biosig.sf.net"
   url "http://sourceforge.net/projects/biosig/files/BioSig%20for%20C_C%2B%2B/src/biosig4c%2B%2B-1.7.4.src.tar.gz"
   version "1.7.4"
-  sha256 "5f3b016a5681112a9078852449833046b39cfa48f253d0765949db0cb2bded3b"  sha256 "c3debc3315dca2ad915bd0e6aac55df29c87b2b2be028f8b8b72a136f1b79c97"
+  sha256 "5f3b016a5681112a9078852449833046b39cfa48f253d0765949db0cb2bded3b"
 
   # depends_on "cmake" => :build
   # depends_on :x11 # if your formula requires any X11/XQuartz components
