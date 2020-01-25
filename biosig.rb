3f3ebc81bde6409a83e0e5579689b5e0bcc88225
Alois Schlögl <alois.schloegl@gmail.com>
2020-01-25 09:58:56 +0100

    [biosig]: add more tests

--- biosig.rb
+++ biosig.rb
@@ -11,7 +11,7 @@ class Biosig < Formula
   depends_on "dcmtk"
   depends_on "libb64"
   depends_on "suite-sparse"
-  depends_on "tinyxml" # if version == "1.9.5"
+  depends_on "tinyxml"
 
   def install
     system "./configure", "--disable-debug",
@@ -24,8 +24,11 @@ class Biosig < Formula
   end
 
   test do
-    system "save2gdf", "-h"
-    system "physicalunits"
-    system "biosig_fhir"
+    system "#{bin}/save2gdf", "-h"
+    assert_match "usage: save2gdf [OPTIONS] SOURCE DEST", shell_output("#{bin}/save2gdf -h | grep ^usage").strip
+    system "#{bin}/physicalunits"
+    assert_match "mV\t4274\t0x10b2\t0.001\tV", shell_output("#{bin}/physicalunits mV").strip
+    system "#{bin}/biosig_fhir"
+    assert_match "biosig_fhir provides fhir binary template for biosignal data", shell_output("#{bin}/biosig_fhir 2>&1 >/dev/null |head -1").strip
   end
 end
