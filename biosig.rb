3a9405c0747972f891e7618d49417524f0db4b21
Your Name <you@example.com>
2020-06-24 21:53:53 +0200

    pkg biosig: upgrade to v2.0.4

--- biosig.rb
+++ biosig.rb
@@ -1,8 +1,8 @@
 class Biosig < Formula
   desc "Tools for biomedical signal processing and data conversion"
   homepage "https://biosig.sourceforge.io"
-  url "https://downloads.sourceforge.net/project/biosig/BioSig%20for%20C_C%2B%2B/src/biosig-2.0.3.src.tar.gz"
-  sha256 "9b072345d2ee4b5c399c3338bb080ecb82a2980c2e2ecf6df8da053d71d281d4"
+  url "https://downloads.sourceforge.net/project/biosig/BioSig%20for%20C_C%2B%2B/src/biosig-2.0.4.src.tar.gz"
+  sha256 "2b2b5d0cdb7a886b7c390d000bb9210b9b6e03f790c6443730dab96496926928"
 
   depends_on "gawk" => :build
   depends_on "gnu-sed" => :build
@@ -12,6 +12,8 @@ class Biosig < Formula
   depends_on "libb64"
   depends_on "suite-sparse"
   depends_on "tinyxml"
+  # depends_on "numpy"  => :optional
+  # depends_on "octave" => :optional
 
   resource "test" do
     url "https://pub.ist.ac.at/~schloegl/download/TEST_44x86_e1.GDF"
@@ -31,9 +33,13 @@ class Biosig < Formula
   test do
     assert_match "usage: save2gdf [OPTIONS] SOURCE DEST", shell_output("#{bin}/save2gdf -h").strip
     assert_match "mV\t4274\t0x10b2\t0.001\tV", shell_output("#{bin}/physicalunits mV").strip
-    assert_match "biosig_fhir provides fhir binary template for biosignal data", shell_output("#{bin}/biosig_fhir 2>&1").strip
+    assert_match "biosig_fhir provides fhir binary template for biosignal data",
+      shell_output("#{bin}/biosig_fhir 2>&1").strip
     testpath.install resource("test")
     assert_match "NumberOfChannels", shell_output("#{bin}/save2gdf -json TEST_44x86_e1.GDF").strip
     assert_match "NumberOfChannels", shell_output("#{bin}/biosig_fhir TEST_44x86_e1.GDF").strip
+    assert_no_match "Error", shell_output("python3 -c 'import biosig'").strip
+    assert_match "mexSLOAD",
+      shell_output("octave --no-gui --norc --eval 'pkg load mexbiosig; which mexSLOAD; exit' ").strip
   end
 end
