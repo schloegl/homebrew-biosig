53f55c944b7332b39d513f9723d1b5fc28c096bb
Your Name <you@example.com>
2020-06-24 20:47:56 +0200

    pkg sigviewer: add patch from upstream

--- sigviewer.rb
+++ sigviewer.rb
@@ -29,3 +29,39 @@ class Sigviewer < Formula
 end
 
 __END__
+commit 714febff135cb6af4cbded2ee1d3c9b15461b9db
+Author: Kevin Smathers <kevin.smathers@hp.com>
+Date:   Thu Jun 4 13:46:35 2020 -0700
+
+    Fixed SEGV when saving to CSV after deleting an event
+
+diff --git a/src/gui_impl/commands/save_gui_command.cpp b/src/gui_impl/commands/save_gui_command.cpp
+index 8c37f8a..4d337e8 100644
+--- a/src/gui_impl/commands/save_gui_command.cpp
++++ b/src/gui_impl/commands/save_gui_command.cpp
+@@ -299,14 +299,17 @@ void SaveGuiCommand::exportEventsToCSV ()
+ 
+         for (unsigned int i = 0; i < event_manager_pt->getNumberOfEvents(); i++)
+         {
+-            row tmp = {
+-                event_manager_pt->getEvent(i)->getPosition(),
+-                event_manager_pt->getEvent(i)->getDuration(),
+-                event_manager_pt->getEvent(i)->getChannel(),
+-                event_manager_pt->getEvent(i)->getType(),
+-                event_manager_pt->getNameOfEvent(i)
+-            };
+-            events.append(tmp);
++            auto evt = event_manager_pt->getEvent(i);
++            if (evt != NULL) {
++                row tmp = {
++                    evt->getPosition(),
++                    evt->getDuration(),
++                    evt->getChannel(),
++                    evt->getType(),
++                    event_manager_pt->getNameOfEvent(i)
++                };
++                events.append(tmp);
++            }
+         }
+ 
+         std::sort(events.begin(),
