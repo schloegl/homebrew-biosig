9a8b9e2cf6f7fe785fd8c814125f8cada5720ae6
Alois SCHLOEGL <alois.schloegl@ist.ac.at>
2021-01-06 18:09:53 +0100

    biosig 2.1.2

--- biosig.rb
+++ biosig.rb
@@ -1,8 +1,8 @@
 class Biosig < Formula
   desc "Tools for biomedical signal processing and data conversion"
   homepage "https://biosig.sourceforge.io"
-  url "https://downloads.sourceforge.net/project/biosig/BioSig%20for%20C_C%2B%2B/src/biosig-2.1.1.src.tar.gz"
-  sha256 "808c6edef3ff7d0afe8bb59eb6cabd3d9023b7077aae84fb90923cbb845e4564"
+  url "https://downloads.sourceforge.net/project/biosig/BioSig%20for%20C_C%2B%2B/src/biosig-2.1.2.src.tar.gz"
+  sha256 "1b5bf62739faf3caef7cb7fbb14c7f1dea352caa548b08c7bb5adaaef2f4d1b4"
   license "GPL-3.0-or-later"
 
   livecheck do
@@ -30,8 +30,6 @@ class Biosig < Formula
     sha256 "75df4a79b8d3d785942cbfd125ce45de49c3e7fa2cd19adb70caf8c4e30e13f0"
   end
 
-  patch :DATA
-
   def install
     system "./configure", "--disable-debug",
            "--disable-dependency-tracking",
@@ -52,35 +50,3 @@ class Biosig < Formula
   end
 end
 
-__END__
-diff --git a/Makefile.in b/Makefile.in
-index de1eaf5c..6a6642ab 100644
---- a/Makefile.in
-+++ b/Makefile.in
-@@ -116,11 +116,11 @@ ifneq (:,@PYTHON3@)
- first biosig4c++/python/dist/Biosig-@PACKAGE_VERSION@.tar.gz ::
- 	-PYTHON=@PYTHON3@ make -C biosig4c++/python dist/Biosig-@PACKAGE_VERSION@.tar.gz
- install :: biosig4c++/python/dist/Biosig-@PACKAGE_VERSION@.tar.gz
--	@PYTHON3@ -m pip install $<
-+	-@PYTHON3@ -m pip install $<
- 	-$(foreach py, $(shell pyversions -i),  $(py) -m pip install $<;)
- 	-$(foreach py, $(shell py3versions -i), $(py) -m pip install $<;)
- uninstall ::
--	@PYTHON3@ -m pip uninstall -y $<
-+	-@PYTHON3@ -m pip uninstall -y $<
- 	-$(foreach py, $(shell pyversions -i),  $(py) -m pip uninstall -y Biosig;)
- 	-$(foreach py, $(shell py3versions -i), $(py) -m pip uninstall -y Biosig;)
- clean ::
-diff --git a/biosig4c++/Makefile.in b/biosig4c++/Makefile.in
-index 0e947b0a..b48c76c1 100644
---- a/biosig4c++/Makefile.in
-+++ b/biosig4c++/Makefile.in
-@@ -979,7 +979,7 @@ uninstall_tools:
- 	${RM} $(DESTDIR)$(bindir)/{heka2itx,bin2rec,rec2bin,save2aecg,save2scp}
- 	${RM} $(DESTDIR)$(bindir)/biosig_fhir${BINEXT}
- 	${RM} $(DESTDIR)$(mandir)/man1/physicalunits.1
--	${RM} $(DESTDIR)$(mandir)/man1/{save2gdf,biosig_fhir}.1
-+	${RM} $(DESTDIR)$(mandir)/man1/{save2gdf,biosig_fhir,biosig2gdf}.1
- 	${RM} $(DESTDIR)$(mandir)/man1/{heka2itx,bin2rec,rec2bin,save2aecg,save2scp}.1
- 
- ### Install mexbiosig for Octave
