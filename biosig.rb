d3e74904d3f3b399fe648292b342c4b738889347
Alois SCHLOEGL <alois.schloegl@ist.ac.at>
2021-07-06 22:29:04 +0200

    biosig 2.3.0

--- biosig.rb
+++ biosig.rb
@@ -1,8 +1,8 @@
 class Biosig < Formula
   desc "Tools for biomedical signal processing and data conversion"
   homepage "https://biosig.sourceforge.io"
-  url "https://downloads.sourceforge.net/project/biosig/BioSig%20for%20C_C%2B%2B/src/biosig-2.1.2.src.tar.gz"
-  sha256 "1b5bf62739faf3caef7cb7fbb14c7f1dea352caa548b08c7bb5adaaef2f4d1b4"
+  url "https://downloads.sourceforge.net/project/biosig/BioSig%20for%20C_C%2B%2B/src/biosig-2.3.0.src.tar.gz"
+  sha256 "9c66d1167628d6c2027168fb82bcd3c28357e59bd5d8b1d9e615457a2eb59ca0"
   license "GPL-3.0-or-later"
 
   livecheck do
@@ -11,17 +11,16 @@ class Biosig < Formula
   end
 
   bottle do
-    cellar :any
-    sha256 "3d9649438fd9e04c97bee4ad9595bfcbbe09ae83f914e8ddd011fef0705b5544" => :big_sur
-    sha256 "7ddfff1529286000cd32a28ce1bf735cfe810804c08b20eaa2fe39a587f8b73b" => :catalina
-    sha256 "4786b282a950d325f91d681615a9d60cc8335703f818d527c5d55f7718b206e9" => :mojave
-    sha256 "0818b0bdfe19286f9d18de35d5fa72981b4b1e1403083c92136c7d5c937dbe6f" => :high_sierra
+    sha256 cellar: :any, big_sur:  "ae96f3a8066f229dec3c3731d038deec95e28178d1484711f2464164d0cd50ad"
+    sha256 cellar: :any, catalina: "c749c7059004f7921be08564486ced1d2ee8d96b53046e3d95a78efd5bce9371"
+    sha256 cellar: :any, mojave:   "d5d9917926fd98e298b74e2171d5b58c2b766e6865cd32e21db4cf9d9935ac0e"
   end
 
   depends_on "gawk" => :build
-  depends_on "gnu-tar" => :build
+  depends_on "libarchive" => :build
   depends_on "dcmtk"
   depends_on "libb64"
+  depends_on "numpy"
   depends_on "suite-sparse"
   depends_on "tinyxml"
 
@@ -49,4 +48,3 @@ class Biosig < Formula
     assert_match "NumberOfChannels", shell_output("#{bin}/biosig_fhir TEST_44x86_e1.GDF").strip
   end
 end
-
